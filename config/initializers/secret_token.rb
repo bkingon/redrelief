2b7fe21df1e0a58ee1ed93087f194ff5a5fa922a
bkingon <bkingon@gmail.com>
2013-01-09 12:35:47 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redrelief::Application.config.secret_token = 'a42cb04e49ade0bb250ae98c069b561ad444527c45280480ef13d30f3568ab02a7e1c6eaedaafcebc3a1725cbd1e257e0a3d4531ef6dddb5b362c535d10eb504'
